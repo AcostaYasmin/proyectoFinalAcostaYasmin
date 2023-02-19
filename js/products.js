b3dbcd4cd84e3f85b71b2d3a43d26647ac4c4a94
Yasmin Acosta <yasminacosta1993@gmail.com>
2023-02-18 23:54:25 -0300

    commit

---
+++ js/products.js
@@ -0,0 +1,28 @@
+
+const productos = [
+    {
+        nombre: "remeras",
+        img:"https://sites.google.com/site/ropaalamodakmsm/home/ropa-ninos" ,
+        precio: 400}, 
+    {
+        nombre: "short",
+        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fblunki.com.ar%2Fcontent%2Frevendedoras&psig=AOvVaw12qRhxPmPNXRkJHQtYMXbi&ust=1676858049833000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiV4JW9oP0CFQAAAAAdAAAAABAR",
+        precio: 500}, 
+    {
+        nombre: "jeans",
+        img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mdzol.com%2Fmdz-femme%2Fmoda%2F2020%2F9%2F1%2Flos-cinco-tips-infalibles-para-aprender-comprar-ropa-de-ninos-102153.html&psig=AOvVaw075zuljW0fR2Hz5B2shvP9&ust=1676858335399000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOD8zp2-oP0CFQAAAAAdAAAAABAJ" ,
+        precio: 900}, 
+    {
+        nombre: "camperas",
+        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Far.pinterest.com%2Fmodainfantilblog%2Fmoda-oto%25C3%25B1o-invierno-en-ropa-para-ni%25C3%25B1os%2F&psig=AOvVaw12qRhxPmPNXRkJHQtYMXbi&ust=1676858049833000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiV4JW9oP0CFQAAAAAdAAAAABAJ",
+        precio: 1000}, 
+    {
+        nombre: "busos", 
+        img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fminibhu.com%2Fblogs%2Fhazlo-con-minibhu%2Focasiones-especiales-ideas-de-ropa-elegante-para-ninos&psig=AOvVaw12qRhxPmPNXRkJHQtYMXbi&ust=1676858049833000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiV4JW9oP0CFQAAAAAdAAAAABAj" ,
+        precio: 1200}, 
+    {
+        nombre: "joguins",
+        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fropatemporada2016%2F&psig=AOvVaw0HLgTTOOyY0ykAT7WggRW2&ust=1676858293900000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCODi6Ym-oP0CFQAAAAAdAAAAABAE",
+        precio: 700},
+];
+
